502de9b6b523114f0d9759e70bf8c36af3eb02cc
imam <up879792@myport.ac.uk>
2025-12-15 21:33:18 +0000

    Update environment configuration and API handler: Changed logo path in .env.example for consistency and updated API export in index.js to support default export handling.

--- api/index.js
+++ api/index.js
@@ -1,3 +1,4 @@
 // Vercel serverless function wrapper
 // This re-exports the built handler from dist/index.js
-module.exports = require('../dist/index.js');
+const handler = require('../dist/index.js');
+module.exports = handler.default || handler;
